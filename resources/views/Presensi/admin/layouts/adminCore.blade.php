44d9bbbe056dd2a4a4e19c413ff98f4f57453753
paksiswanto <unitedkuryu@gmail.com>
2024-10-30 23:46:04 +0700

    cek

--- resources/views/Presensi/admin/layouts/adminCore.blade.php
+++ resources/views/Presensi/admin/layouts/adminCore.blade.php
@@ -63,7 +63,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('dashboardAdmin') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-aperture"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10zm-2 2V9l8-6l8 6v12h-7v-6h-2v6zm8-8.75"/></svg>
                                 </span>
                                 <span class="hide-menu">Dhasboard</span>
                             </a>
@@ -71,7 +71,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('presences') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-shopping-cart"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linejoin="round" d="m5.5 6.219l2 2l3.5-3.5M9 13.5h2m-2 0v-3m0 3H7m2-3h5.5v-8h-13v8H7m2 0H7m-2 3h2m0 0v-3"/></svg>
                                 </span>
                                 <span class="hide-menu">Absen</span>
                             </a>
@@ -79,7 +79,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('Jurusan.index') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-currency-dollar"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 16.175q.25-.075.488-.125T6 16h1V4H6q-.425 0-.712.288T5 5zM6 22q-1.25 0-2.125-.875T3 19V5q0-1.25.875-2.125T6 2h7v2H9v12h6v-3h2v5H6q-.425 0-.712.288T5 19t.288.713T6 20h13v-8h2v10zm-1-5.825V4zM17.5 12q0-2.3 1.6-3.9T23 6.5q-2.3 0-3.9-1.6T17.5 1q0 2.3-1.6 3.9T12 6.5q2.3 0 3.9 1.6t1.6 3.9"/></svg>
                                 </span>
                                 <span class="hide-menu">Jurusan</span>
                             </a>
@@ -87,7 +87,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('level.index') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-cpu"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M23 2H1a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1h22a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1m-1 18h-2v-1h-5v1H2V4h20zM10.29 9.71A1.71 1.71 0 0 1 12 8c.95 0 1.71.77 1.71 1.71c0 .95-.76 1.72-1.71 1.72s-1.71-.77-1.71-1.72m-4.58 1.58c0-.71.58-1.29 1.29-1.29a1.29 1.29 0 0 1 1.29 1.29c0 .71-.58 1.28-1.29 1.28S5.71 12 5.71 11.29m10 0A1.29 1.29 0 0 1 17 10a1.29 1.29 0 0 1 1.29 1.29c0 .71-.58 1.28-1.29 1.28s-1.29-.57-1.29-1.28M20 15.14V16H4v-.86c0-.94 1.55-1.71 3-1.71c.55 0 1.11.11 1.6.3c.75-.69 2.1-1.16 3.4-1.16s2.65.47 3.4 1.16c.49-.19 1.05-.3 1.6-.3c1.45 0 3 .77 3 1.71"/></svg>
                                 </span>
                                 <span class="hide-menu">Kelas</span>
                             </a>
@@ -95,7 +95,7 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('dispen.indexAdmin') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-activity-heartbeat"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 15h7l2-2H6zm0-4h6V9H6zM4 7v10h7l-2 2H2V5h20v3h-2V7zm18.9 5.3q.125.125.125.275t-.125.275l-.9.9L20.25 12l.9-.9q.125-.125.275-.125t.275.125zM13 21v-1.75l6.65-6.65l1.75 1.75L14.75 21zM4 7v10z"/></svg>
                                 </span>
                                 <span class="hide-menu">Izin</span>
                             </a>
@@ -103,15 +103,15 @@
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('list.userAdmin') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-playlist"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.775 12q-.9 0-1.5-.675T7.8 9.75l.325-2.45q.2-1.425 1.3-2.363T12 4t2.575.938t1.3 2.362l.325 2.45q.125.9-.475 1.575t-1.5.675zm0-2h4.45L13.9 7.6q-.1-.7-.637-1.15T12 6t-1.263.45T10.1 7.6zM4 20v-2.8q0-.85.438-1.562T5.6 14.55q1.55-.775 3.15-1.162T12 13t3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20zm2-2h12v-.8q0-.275-.137-.5t-.363-.35q-1.35-.675-2.725-1.012T12 15t-2.775.338T6.5 16.35q-.225.125-.363.35T6 17.2zm6 0"/></svg>
                                 </span>
-                                <span class="hide-menu">User</span>
+                                <span class="hide-menu">Hadir</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="{{ route('confirmUser') }}" aria-expanded="false">
                                 <span>
-                                    <i class="ti ti-playlist"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M4 12v-7c0 -0.55 0.45 -1 1 -1h14c0.55 0 1 0.45 1 1v14c0 0.55 -0.45 1 -1 1h-14c-0.55 0 -1 -0.45 -1 -1Z"/><path stroke-dasharray="14" stroke-dashoffset="14" d="M8 12l3 3l5 -5"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="14;0"/></path></g></svg>
                                 </span>
                                 <span class="hide-menu">Konfirmasi</span>
                             </a>
